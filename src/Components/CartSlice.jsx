89dee280d1814664680744849365addda39b867b
Yousef <36880618+yusnof@users.noreply.github.com>
2024-07-23 14:40:28 +0200

    Not done yet

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
 
-const CartSlice = ({
- 
+//Creating CarSlice object and the logic of the adding/removing function. 
+const CartSlice = createSlice({
+    name:"cart",
+    initialState,
+    
+    //logic behind some functions behind the cart
+    reducers:{
+        addItemToCart(state,action){
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id); // returns a bool if found or not
+            if(existingItem){
+                existingItem.quantity +=1; 
+            }
+            else{
+                existingItem.cartItems.push({...action.payload, quantity:1})
+            }
+        }
+    },
+        removeItemFromCart(state,action){
+            state.cartItems = state.cartItems.fliter(item => item.id !== action.payload); //updates the array with filter function.
+    },
+    clearCart(state){
+
+    },
+    increaseItemQuantity(state, action) {
+        const itemToDecrease = state.cartItems.find(item => item.id === action.payload); // returns a bool
+        if(itemToDecrease && itemToDecrease.quantity > 1){
+            itemToDecrease.quantity -= 1;
+        }
+
+
+    }
 });
 
+const initialState = {
+    cartItems: [],
+  };
 
+
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+  } = CartSlice.actions;
+export default CartSlice.reducer;
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch} from 'react-redux';
+import { useState } from  'react';
+import {addItemToCart} from "./CartSlice"; 
 
 const ProductList = () => {
 
@@ -8,11 +11,26 @@ const ProductList = () => {
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+  };
 
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
+        {products.map(product =>( 
+          <li key={product.id} className="product-list-item">
+            <span>{product.name} - ${product.price}</span>
+            <button className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+            onClick={() => handleAddToCart(product)}
+            disabled={disabledProducts.includes(product.id)} >Add to Cart</button>
+          </li>
+        ))}
      
       </ul>
     </div>
